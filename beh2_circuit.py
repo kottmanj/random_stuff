d433521e21c25b6b125ec944336d10d375fd0469
Jakob Kottmann <jakob.kottmann@gmail.com>
2022-10-10 06:50:11 -0400

    update

--- beh2_circuit.py
+++ beh2_circuit.py
@@ -4,4 +4,3 @@ mol = tq.Molecule(geometry="be 0.0 0.0 0.0\n H 0.0 0.0 1.0\nH 0.0 0.0 -1.0", bas
 
 U = mol.make_ansatz(name="SPA", edges=[(0,1,2),(3,4,5)])
 U.export_to(filename="spa_beh2.pdf")
-U.export_to(filename="spa_beh2.png")
